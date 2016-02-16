55a9a17d8d6e2d95cf49ca4fe596d4fa5f0143da
Lauren Nicole Roth <laurenroth@gmail.com>
2016-02-16 15:16:55 -0500

    Add CardView skeleton

---
+++ LuvCur/CardView.swift
@@ -0,0 +1,49 @@
+//
+//  CardView.swift
+//  LuvCur
+//
+//  Created by Lauren Nicole Roth on 2/16/16.
+//  Copyright © 2016 LuvCur. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CardView: UIView
+{
+    // MARK - Properties
+    private let imageView: UIImageView = UIImageView()
+    private let nameLabel: UILabel = UILabel()
+    
+    var name: String? {
+        didSet {
+            if let name = name {
+                nameLabel.text = name
+            }
+        }
+    }
+    
+    var image: UIImage? {
+        didSet {
+            if let image = image {
+                imageView.image = image
+            }
+        }
+    }
+    
+    // MARK - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+    
+    private func initialize(){
+        addSubview(imageView)
+        addSubview(nameLabel)
+    }
+}
---
+++ User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  LuvCur
+//
+//  Created by Lauren Nicole Roth on 2/16/16.
+//  Copyright © 2016 LuvCur. All rights reserved.
+//
+
+import Foundation
