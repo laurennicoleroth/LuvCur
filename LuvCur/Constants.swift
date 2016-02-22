dc60c8dd32fb704bf2e17483b652284ddd2fa579
Lauren Nicole Roth <laurenroth@gmail.com>
2016-02-22 13:57:42 -0500

    Pod install parse and add app keys

--- LuvCur/Constants.swift
+++ LuvCur/Constants.swift
@@ -8,6 +8,6 @@
 
 import Foundation
 
-let parseAppID = "XXX"
-let parseClientKey = "YYY"
+let parseAppID = "PjWmtkHRJLfz1ncO1U0ZVmur3xWA0CMWrZJoUa14"
+let parseClientKey = "19pTLVS7gnNCLITauMYRNYgSBbxfEPnTrEX8R3l2"
 let firebaseAppURL = "https://luvcurios.firebaseio.com/"
